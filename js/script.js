cd81aac0910d3066917cf55bb4538638234347f0
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-01-21 18:06:24 +0100

    milestone 2 end

--- js/script.js
+++ js/script.js
@@ -71,7 +71,7 @@ const myIcons = [
   {
     nome: 'lemon',
     prefisso:'fa-',
-    tipo:'animale',
+    tipo:'animal',
     famiglia:'fas',
   },
   {
@@ -107,6 +107,8 @@ const myIcons = [
 
 ];
 
+const iconsColor = ['blue', 'orange', 'purple'];
+
 myIcons.forEach((item) => {
   const {nome, prefisso, famiglia} = item;
 
@@ -117,3 +119,34 @@ myIcons.forEach((item) => {
   </div>
   `
 });
+
+
+/*
+Milestone 2:
+- Definire un array di colori e associare ad ogni tipo di icona un colore.
+- Visualizzare le icone di colore diverso in base al tipo.
+
+*/
+const type =[];
+
+myIcons.forEach((item) => {
+  if (!type.includes(item.tipo)) {
+    type.push(item.tipo);
+  }
+});
+
+myIcons.forEach((item) => {
+  const indexOfType = type.indexOf(item.tipo)
+
+  if (indexOfType != -1) {
+    item.color = iconsColor[indexOfType];
+  }
+});
+
+/*
+Milestone 3:
+- Aggiungere una select per filtrare le icone in base al tipo.
+- Popolare le options della select dinamicamente e,
+ogni volta che cambia il valore selezionato,
+visualizzare le icone corrispondenti.
+*/
